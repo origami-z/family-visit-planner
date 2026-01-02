1201ce1103ec55c4b940a77d2db65a58fa684284
origami-z <5257855+origami-z@users.noreply.github.com>
2026-01-02 15:36:44 +0000

    [chat] try use hash history for SPA deploy

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
-import { createRouter } from '@tanstack/react-router'
+import { createHashHistory, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
 // Create a new router instance
 export const getRouter = () => {
+  const history = createHashHistory()
   const router = createRouter({
     routeTree,
+    history,
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
   })
